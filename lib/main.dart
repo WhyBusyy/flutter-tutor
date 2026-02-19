99ef1b6462834730c569d1cc6a6bd2c4852f41bb
WhyBusyy <ybg6152@naver.com>
2026-02-19 23:08:59 +0900

    기본 위젯 이해하기 + 로그인 화면

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,38 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(appBar: AppBar()),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Hello, Flutter', style: TextStyle(fontSize: 28)),
+          centerTitle: true,
+        ),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.all(16),
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(32),
+                  child: Image.network(
+                    "https://devclass.devstory.co.kr/flutter-basic/1/cat.png",
+                    width: 81,
+                  ),
+                ),
+                TextField(decoration: InputDecoration(labelText: '이메일')),
+                TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(labelText: '비밀번호'),
+                ),
+                Container(
+                  width: double.infinity,
+                  margin: EdgeInsets.only(top: 16),
+                  child: ElevatedButton(onPressed: () {}, child: Text('로그인')),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
