36abcf7f308cf1993106cca4b5fadad112d7c5e6
ByungGyu-Yu <ybg2024@platpharm.co.kr>
2026-02-24 20:00:33 +0900

    Update analysis options, dependencies, and iOS configurations; refactor main app structure and UI components for improved functionality and design.

--- flutter-section1/lib/main.dart
+++ flutter-section1/lib/main.dart
@@ -1,121 +1,253 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      home: HomePage(), // 홈페이지 보여주기
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    // 음식 사진 데이터
+    List<Map<String, dynamic>> dataList = [
+      {
+        "category": "수제버거",
+        "imgUrl":
+            "https://devclass.devstory.co.kr/flutter-basic/1/food-burger.jpg",
+      },
+      {
+        "category": "건강식",
+        "imgUrl":
+            "https://devclass.devstory.co.kr/flutter-basic/1/food-soup.jpg",
+      },
+      {
+        "category": "한식",
+        "imgUrl":
+            "https://devclass.devstory.co.kr/flutter-basic/1/food-korean-meals.jpg",
+      },
+      {
+        "category": "디저트",
+        "imgUrl":
+            "https://devclass.devstory.co.kr/flutter-basic/1/food-tiramisu.jpg",
+      },
+      {
+        "category": "피자",
+        "imgUrl":
+            "https://devclass.devstory.co.kr/flutter-basic/1/food-pizza.jpg",
+      },
+      {
+        "category": "볶음밥",
+        "imgUrl":
+            "https://devclass.devstory.co.kr/flutter-basic/1/food-pizza.jpg",
+      },
+    ];
+
+    // 화면에 보이는 영역
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        elevation: 0, // 그림자 없애기
+        backgroundColor: Colors.white, // 배경 색상
+        centerTitle: false, // title 중앙 정렬
+        iconTheme: IconThemeData(color: Colors.black), // app bar icon color
+        title: Text(
+          "Food Recipe",
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 28,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        actions: [
+          /// My Button
+          IconButton(
+            onPressed: () {
+              print("go my page");
+            },
+            icon: Icon(Icons.person_outline),
+          ),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: Column(
+        children: [
+          /// 검색
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: "상품을 검색해주세요.",
+                border: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.black),
+                ),
+                // 돋보기 아이콘
+                suffixIcon: IconButton(
+                  icon: Icon(Icons.search),
+                  onPressed: () {
+                    print("돋보기 아이콘 클릭");
+                  },
+                ),
+              ),
+            ),
+          ),
+          Divider(height: 1),
+
+          /// 카테고리 목록
+          Expanded(
+            child: ListView.builder(
+              // 보여주려는 데이터 개수
+              itemCount: dataList.length,
+              // itemCount 만큼 반복되며 화면에 보여주려는 위젯
+              // index가 0부터 dataList.length - 1까지 증가하며 전달됩니다.
+              itemBuilder: (context, index) {
+                // dataList에서 index에 해당하는 data 꺼내기
+                Map<String, dynamic> data = dataList[index];
+                String category = data["category"];
+                String imgUrl = data["imgUrl"];
+
+                // 카드 형태의 위젯
+                return Card(
+                  margin: const EdgeInsets.all(8),
+                  // 위젯들을 위로 중첩하기 위해 Stack 위젯 사용
+                  child: Stack(
+                    alignment: Alignment.center, // 중앙 정렬
+                    children: [
+                      /// 배경 이미지
+                      Image.network(
+                        imgUrl,
+                        width: double.infinity, // 가득 채우기
+                        height: 120,
+                        fit: BoxFit
+                            .cover, // 이미지 비율을 유지하며 주어진 width와 height를 가득 채우기
+                      ),
+
+                      /// 배경 위에 글씨가 보이도록 반투명한 박스 추가
+                      Container(
+                        width: double.infinity,
+                        height: 120,
+                        color: Colors.black.withOpacity(0.5), // 투명도 50%
+                      ),
+
+                      /// 카테고리
+                      Text(
+                        category,
+                        style: TextStyle(color: Colors.white, fontSize: 36),
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+
+      /// AppBar 왼쪽에 아이콘을 누를 때 왼쪽에서 오른쪽으로 나오는 위젯
+      /// 해당 위젯을 추가하면 AppBar 왼쪽에 아이콘이 자동으로 추가됨
+      drawer: Drawer(
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: .center,
           children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            // Drawer 윗 부분
+            DrawerHeader(
+              margin: const EdgeInsets.all(0),
+              decoration: BoxDecoration(color: Colors.amber),
+              child: SizedBox(
+                width: double.infinity, // 가득 채우기
+                child: Column(
+                  children: [
+                    /// 동그란 배경
+                    CircleAvatar(
+                      radius: 36,
+                      backgroundColor: Colors.white,
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+
+                        /// 이미지
+                        child: Image.network(
+                          "https://devclass.devstory.co.kr/flutter-basic/1/cat.png",
+                          width: 62,
+                        ),
+                      ),
+                    ),
+
+                    /// 닉네임 상단 여백
+                    SizedBox(height: 13),
+
+                    /// 닉네임
+                    Text(
+                      "닉네임",
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+
+                    /// 이메일
+                    Text(
+                      "hello@world.com",
+                      style: TextStyle(fontSize: 16, color: Colors.black),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+
+            /// 이벤트 배너
+            // 특정 비율로 위젯을 보여주기
+            AspectRatio(
+              aspectRatio: 12 / 4, // 가로 : 세로 = 12 : 4 (이미지를 12:4로 준비했어요)
+              /// 이벤트 배너 리스트
+              /// 스크롤을 하는데, 특정 항목이 스냅이 걸리도록 만들고 싶은 경우 PageView를 사용해요.
+              child: PageView(
+                children: [
+                  Image.network(
+                    "https://devclass.devstory.co.kr/flutter-basic/1/sale-event-banner1.jpg",
+                  ),
+                  Image.network(
+                    "https://devclass.devstory.co.kr/flutter-basic/1/sale-event-banner2.jpg",
+                  ),
+                  Image.network(
+                    "https://devclass.devstory.co.kr/flutter-basic/1/sale-event-banner3.jpg",
+                  ),
+                  Image.network(
+                    "https://devclass.devstory.co.kr/flutter-basic/1/sale-event-banner4.jpg",
+                  ),
+                ],
+              ),
+            ),
+
+            /// 구매 내역
+            ListTile(
+              title: Text('구매내역', style: TextStyle(fontSize: 18)),
+              trailing: Icon(Icons.arrow_forward_ios, color: Colors.black),
+              onTap: () {
+                // 클릭시 drawer 닫기
+                Navigator.pop(context);
+              },
+            ),
+
+            /// 레시피 공유하기
+            ListTile(
+              title: Text('저장한 레시피', style: TextStyle(fontSize: 18)),
+              trailing: Icon(Icons.arrow_forward_ios, color: Colors.black),
+              onTap: () {
+                // 클릭시 drawer 닫기
+                Navigator.pop(context);
+              },
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
